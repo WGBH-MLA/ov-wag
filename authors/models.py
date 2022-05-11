937d2ad45fa2801f1688d0b49e2cf492fc6d5382
Harpo <harpo@harpo.me>
2022-05-11 12:39:41 -0700

    Adds Author model
    * Add an Author model having a many-to-many association with ExhibitPage models.
    * Adds an API endpoint at `api/v2/authors` for fetching Authors.
    * Adds authors data as part of ExhibitPage api responses (array of author objects).
    * Adds ability to edit Authors in the wagtail admin interface.
    * Adds ability to choose Authors to associate with ExhibitPages.
    
    Closes #17. Closes #18.

---
+++ authors/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from wagtail.api import APIField
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Orderable
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, RichTextFieldPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.models import register_snippet
+from modelcluster.fields import ParentalKey
+
+
+class AuthorsOrderable(Orderable):
+    page = ParentalKey('exhibit.ExhibitPage', related_name='authors', null=True)
+    author = models.ForeignKey(
+        'authors.Author',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    panels = [SnippetChooserPanel('author')]
+
+
+class Author(models.Model):
+    """Author of a page"""
+
+    name = models.CharField(max_length=100, help_text='Author name')
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    bio = RichTextField(blank=True, help_text='Brief author bio')
+
+    panels = [
+        MultiFieldPanel(
+            [FieldPanel('name'), ImageChooserPanel('image'), RichTextFieldPanel('bio')]
+        )
+    ]
+
+    api_fields = [
+        APIField('name'),
+        APIField('image'),
+        APIField('bio'),
+    ]
+
+    def __str__(self):
+        """str representation of this Author"""
+        return self.name
+
+
+register_snippet(Author)
