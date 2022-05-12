797fd4af11e1f8270ee61296a150b5dbee3e88ba
Harpo <harpo@harpo.me>
2022-05-12 09:49:32 -0700

    Cover image + hero image (#20)
    * auto reload
    
    * Adds hero_image field with content panel and APIField
    
    * Mount volume in ./ov cmd
    
    * add hero_thumb
    
    * added migrations
    
    * add Exhibit edit panel for Authors

--- exhibit/models.py
+++ exhibit/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.api.fields import ImageRenditionField
 from wagtail.search import index
 from wagtail.api import APIField
 from pydantic import BaseModel
@@ -22,6 +23,8 @@ class ExhibitPageApiSchema(BaseModel):
     title: str
     body: str
     cover_image: ImageApiSchema
+    hero_image: ImageApiSchema
+    hero_thumb: ImageApiSchema
 
 
 class ExhibitPage(Page):
@@ -34,14 +37,37 @@ class ExhibitPage(Page):
         on_delete=models.SET_NULL,
         related_name='+',
     )
-
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
     search_fields = Page.search_fields + [
         index.SearchField('body'),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        FieldPanel('cover_image', classname="full"),
+        FieldPanel('hero_image', classname="full"),
         MultiFieldPanel([InlinePanel('authors', label='Author', heading='Author(s)')]),
     ]
 
-    api_fields = [APIField('body'), APIField('cover_image'), APIField('authors')]
+    api_fields = [
+        APIField('body'),
+        APIField(
+            'cover_image',
+            serializer=ImageRenditionField('fill-1600x500'),
+        ),
+        APIField(
+            'hero_image',
+            serializer=ImageRenditionField('fill-1920x1080'),
+        ),
+        APIField(
+            'hero_thumb',
+            serializer=ImageRenditionField('fill-480x270', source='hero_image'),
+        ),
+        APIField('authors'),
+    ]
