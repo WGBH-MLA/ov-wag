cc806ea4131b4c36ad5d57351c6bf68bcdeab822
Harpo <harpo@harpo.me>
2022-06-03 11:38:55 -0700

    Create ExhibitAPIViewSet with custom listing_default_fields

--- ov_wag/api.py
+++ ov_wag/api.py
@@ -3,6 +3,7 @@ from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.views import ImagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from authors.api import AuthorsAPIViewSet
+from exhibit.api import ExhibitAPIViewSet
 
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
@@ -15,3 +16,4 @@ api_router.register_endpoint('pages', PagesAPIViewSet)
 api_router.register_endpoint('images', ImagesAPIViewSet)
 api_router.register_endpoint('documents', DocumentsAPIViewSet)
 api_router.register_endpoint('authors', AuthorsAPIViewSet)
+api_router.register_endpoint('exhibit', ExhibitAPIViewSet)
