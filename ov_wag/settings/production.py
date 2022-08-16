09be3c2cac5fe84568ef97453e3f7b54ea2c2f98
Harpo <ryan_harbert@wgbh.org>
2022-08-16 14:12:19 -0700

    Set productions settings from env

--- ov_wag/settings/production.py
+++ ov_wag/settings/production.py
@@ -1,6 +1,11 @@
 from .base import *
+from os import environ
 
 DEBUG = False
+ALLOWED_HOSTS = os.environ.get('OV_ALLOWED_HOSTS').split(',')
+WAGTAIL_BASE_URL = os.environ.get('OV_BASE_URL')
+
+SECRET_KEY = os.environ.get('OV_SECRET_KEY')
 
 try:
     from .local import *
