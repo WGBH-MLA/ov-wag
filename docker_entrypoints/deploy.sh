bad37af131a81aa1e20f6c0b8db96a9ce49fd400
Harpo <ryan_harbert@wgbh.org>
2022-08-26 10:51:43 -0700

    🎭 Production settings (#50)
    * Set productions settings from env
    
    * Enable production settings in wsgi.py
    
    * Add file logging to production. Possible fix for #49
    
    * Add CSRF_TRUSTED_ORIGINS setting
    
    * Parse OV_TRUSTED_ORIGINS as comma separated list
    
    * Mount db volume with Z option for selinux
    
    * Build static files on deploy

--- docker_entrypoints/deploy.sh
+++ docker_entrypoints/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-gunicorn ov_wag.wsgi:application --reload
+# Build static files for the admin site
+python3 manage.py collectstatic --noinput
+
+# Run the production server
+gunicorn ov_wag.wsgi:application \
+  --reload \
+  --access-logfile /logs/access.log \
+  --error-logfile /logs/error.log
--- ov_wag/settings/production.py
+++ ov_wag/settings/production.py
@@ -1,6 +1,13 @@
 from .base import *
+from os import environ
 
 DEBUG = False
+ALLOWED_HOSTS = os.environ.get('OV_ALLOWED_HOSTS').split(',')
+WAGTAIL_BASE_URL = os.environ.get('OV_BASE_URL')
+
+CSRF_TRUSTED_ORIGINS = os.environ.get('OV_TRUSTED_ORIGINS').split(',')
+
+SECRET_KEY = os.environ.get('OV_SECRET_KEY')
 
 try:
     from .local import *
