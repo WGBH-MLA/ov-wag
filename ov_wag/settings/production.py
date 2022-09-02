80f7da699a291872160f1e3e3f1b1d5889ed75af
Harpo <ryan_harbert@wgbh.org>
2022-09-02 08:40:22 -0700

    Add admin base url setting

--- ov_wag/settings/production.py
+++ ov_wag/settings/production.py
@@ -4,6 +4,7 @@ from os import environ
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('OV_ALLOWED_HOSTS').split(',')
 WAGTAIL_BASE_URL = os.environ.get('OV_BASE_URL')
+WAGTAILADMIN_BASE_URL = os.environ.get('OV_ADMIN_BASE_URL')
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('OV_TRUSTED_ORIGINS').split(',')
 
