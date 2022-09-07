3ffb3e5e8de3afef03efc1c4c17354f5b6cf2aef
Harpo <ryan_harbert@wgbh.org>
2022-09-07 11:13:08 -0700

    Base ExhibitsOrderable, with migration

--- exhibit/models.py
+++ exhibit/models.py
@@ -1,12 +1,43 @@
 from django.db import models
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.api.fields import ImageRenditionField
 from wagtail.search import index
 from wagtail.api import APIField
 from pydantic import BaseModel
-from ov_wag.serializers import RichTextSerializer
+from modelcluster.fields import ParentalKey
+from ov_wag.serializers import RichTextSerializer, ImageSerializedField
+
+
+class ExhibitsOrderable(Orderable):
+    page = ParentalKey('exhibit.ExhibitPage', related_name='other_exhibits', null=True)
+    exhibit = models.ForeignKey(
+        'exhibit.ExhibitPage',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    panels = [FieldPanel('exhibit')]
+
+    @property
+    def title(self):
+        return self.exhibit.title
+
+    @property
+    def cover_image(self):
+        return self.exhibit.cover_image
+
+    @property
+    def authors(self):
+        return self.exhibit.authors
+
+    api_fields = [
+        APIField('title'),
+        APIField('cover_image', serializer=ImageSerializedField()),
+        APIField('authors'),
+    ]
 
 
 class ImageApiSchema(BaseModel):
@@ -43,6 +74,7 @@ class ExhibitPage(Page):
         on_delete=models.SET_NULL,
         related_name='+',
     )
+
     search_fields = Page.search_fields + [
         index.SearchField('body'),
     ]
@@ -52,6 +84,13 @@ class ExhibitPage(Page):
         FieldPanel('cover_image', classname='full'),
         FieldPanel('hero_image', classname='full'),
         MultiFieldPanel([InlinePanel('authors', label='Author', heading='Author(s)')]),
+        MultiFieldPanel(
+            [
+                InlinePanel(
+                    'other_exhibits', label='Other Exhibits', heading='Other Exhibits'
+                )
+            ]
+        ),
     ]
 
     api_fields = [
@@ -74,4 +113,5 @@ class ExhibitPage(Page):
             serializer=ImageRenditionField('fill-480x270', source='hero_image'),
         ),
         APIField('authors'),
+        APIField('other_exhibits'),
     ]
