d88f2856ec70fc1b269a5ee8fbb590c60ee3ae87
Harpo <ryan_harbert@wgbh.org>
2022-09-19 08:01:46 -0700

    🏰 Base url settings (#56)
    * Revert to backend base image for dev
    
    * Move OV_BASE_URL settings to from production.py to base.py
    
    * Default to empty string if no admin base url - fixes tests

--- ov_wag/settings/base.py
+++ ov_wag/settings/base.py
@@ -173,3 +173,6 @@ WAGTAILSEARCH_BACKENDS = {
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILAPI_BASE_URL = os.environ.get('OV_API_URL')
+
+WAGTAIL_BASE_URL = os.environ.get('OV_BASE_URL')
+WAGTAILADMIN_BASE_URL = os.environ.get('OV_ADMIN_BASE_URL', '')
--- ov_wag/settings/production.py
+++ ov_wag/settings/production.py
@@ -6,8 +6,6 @@ DEBUG = False
 SECRET_KEY = os.environ.get('OV_SECRET_KEY')
 
 ALLOWED_HOSTS = os.environ.get('OV_ALLOWED_HOSTS').split(',')
-WAGTAIL_BASE_URL = os.environ.get('OV_BASE_URL')
-WAGTAILADMIN_BASE_URL = os.environ.get('OV_ADMIN_BASE_URL')
 
 CSRF_TRUSTED_ORIGINS = os.environ.get('OV_TRUSTED_ORIGINS').split(',')
 CSRF_COOKIE_SECURE = True
