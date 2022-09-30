fb299e0562532e7e7975fc3bf0246e2aabefc68a
Harpo <ryan_harbert@wgbh.org>
2022-09-30 13:17:55 -0700

    Add author_id, exhibit_id to api serializers

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,11 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from wagtail.images.api.fields import ImageRenditionField
 from authors.models import Author
 
 
 class AuthorSerializer(ModelSerializer):
     image = ImageRenditionField('fill-100x100')
+    author_id = PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Author
-        fields = ['name', 'image']
+        fields = [
+            'author_id',
+            'name',
+            'image',
+        ]
