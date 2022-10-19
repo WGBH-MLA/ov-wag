2dd1333123273124b440fda07e833a9a2e442532
Harpo <ryan_harbert@wgbh.org>
2022-10-19 14:21:31 -0700

    Add labels and icons to inner blocks

--- ov_collections/models.py
+++ ov_collections/models.py
@@ -15,15 +15,43 @@ class Collection(Page):
 
     content = StreamField(
         [
-            ('interviews', ListBlock(ContentImageBlock(), icon='openquote')),
-            ('archival_footage', ListBlock(ContentImageBlock(), icon='form')),
-            ('photographs', ListBlock(ContentImageBlock(), icon='image')),
+            (
+                'interviews',
+                ListBlock(
+                    ContentImageBlock(label='Interview', icon='openquote'),
+                    icon='openquote',
+                ),
+            ),
+            (
+                'archival_footage',
+                ListBlock(ContentImageBlock(label='Footage', icon='form'), icon='form'),
+            ),
+            (
+                'photographs',
+                ListBlock(
+                    ContentImageBlock(label='Photograph', icon='image'), icon='image'
+                ),
+            ),
             (
                 'original_footage',
-                ListBlock(ContentImageBlock(), icon='doc-full-inverse'),
+                ListBlock(
+                    ContentImageBlock(label='Footage', icon='doc-full-inverse'),
+                    icon='doc-full-inverse',
+                ),
             ),
-            ('programs', ListBlock(ContentBlock(), icon='clipboard-list')),
-            ('related_content', ListBlock(ContentBlock(), icon='list-ul')),
+            (
+                'programs',
+                ListBlock(
+                    ContentBlock(label='Program', icon='clipboard-list'),
+                    icon='clipboard-list',
+                ),
+            ),
+            (
+                'related_content',
+                ListBlock(
+                    ContentBlock(label='Content', icon='list-ul'), icon='list-ul'
+                ),
+            ),
             ('credits', RichTextBlock()),
             ('heading', CharBlock(form_classname='title')),
             ('text', TextBlock()),
