c535d2647dfe22c130a9be81dc6830b003ac7749
Harpo Harbert <ryan_harbert@wgbh.org>
2023-11-07 15:28:35 -0800

    Binds prod server to 8000

--- docker_entrypoints/deploy.sh
+++ docker_entrypoints/deploy.sh
@@ -5,6 +5,9 @@ python3 manage.py collectstatic --noinput
 
 # Run the production server
 gunicorn ov_wag.wsgi:application \
+  -b 0.0.0.0:8000 \
+  --workers 2 \
+  --forwarded-allow-ips '*' \
   --access-logfile - \
   --error-logfile -
   # --access-logfile /logs/access.log \
