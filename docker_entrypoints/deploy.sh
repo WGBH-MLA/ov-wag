9612f171999951d070025c19097f076b6d4b0acd
Harpo Harbert <ryan_harbert@wgbh.org>
2023-11-07 15:23:34 -0800

    Try deploy with pwd /app

--- docker_entrypoints/deploy.sh
+++ docker_entrypoints/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+cd /app/
 # Build static files for the admin site
 python3 manage.py collectstatic --noinput
 
 # Run the production server
 gunicorn ov_wag.wsgi:application \
-  --reload \
-  --access-logfile /logs/access.log \
-  --error-logfile /logs/error.log
+  --access-logfile - \
+  --error-logfile -
+  # --access-logfile /logs/access.log \
+  # --error-logfile /logs/error.log
