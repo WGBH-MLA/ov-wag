61f20a8d8f51886b79b1aa5eddd794e6a2fba758
Harpo Harbert <ryan_harbert@wgbh.org>
2023-11-08 15:57:15 -0800

    Try with QUERYSTRING_AUTH

--- ov_wag/settings/production.py
+++ ov_wag/settings/production.py
@@ -26,6 +26,7 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = True
 
 with suppress(ImportError):
     from .local import *  # noqa F403
