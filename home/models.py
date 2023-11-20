4a1286180d22660f90ea7e414f9fe44c8fea6ae2
Harpo <ryan_harbert@wgbh.org>
2023-11-20 15:21:42 -0800

    👁 Preview (#115)
    * Initial preview WIP
    
    * Fixes lint and test errors
    
    * Use OV_PREVIEW_URL for CLIENT_URLS

--- home/models.py
+++ home/models.py
@@ -4,9 +4,10 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
 from wagtail.fields import RichTextField
 from wagtail.models import Page
+from wagtail_headless_preview.models import HeadlessMixin
 
 
-class HomePage(Page):
+class HomePage(HeadlessMixin, Page):
     body = RichTextField(blank=True)
 
     content_panels: ClassVar[list[FieldPanel]] = [
