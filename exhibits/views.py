ee05915320a2b1a2668ee17a1b8994eb78eec9fe
Harpo Harbert <ryan_harbert@wgbh.org>
2024-02-16 08:44:27 -0800

    Defaults to PagesAPIViewSet

--- exhibits/views.py
+++ exhibits/views.py
@@ -1,15 +1,15 @@
 from typing import ClassVar
 
-from wagtail.api.v2.views import BaseAPIViewSet
+from wagtail.api.v2.views import PagesAPIViewSet
 
 from .models import ExhibitPage
 
 
-class ExhibitsAPIViewSet(BaseAPIViewSet):
+class ExhibitsAPIViewSet(PagesAPIViewSet):
     model = ExhibitPage
 
     listing_default_fields: ClassVar[list[str]] = [
-        *BaseAPIViewSet.listing_default_fields,
+        *PagesAPIViewSet.listing_default_fields,
         'title',
         'cover_image',
         'cover_thumb',
--- ov_collections/views.py
+++ ov_collections/views.py
@@ -1,15 +1,15 @@
 from typing import ClassVar
 
-from wagtail.api.v2.views import BaseAPIViewSet
+from wagtail.api.v2.views import PagesAPIViewSet
 
 from .models import Collection
 
 
-class CollectionAPIViewSet(BaseAPIViewSet):
+class CollectionAPIViewSet(PagesAPIViewSet):
     model = Collection
 
     listing_default_fields: ClassVar[list[str]] = [
-        *BaseAPIViewSet.listing_default_fields,
+        *PagesAPIViewSet.listing_default_fields,
         'title',
         'introduction',
         'cover_image',
