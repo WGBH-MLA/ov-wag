9785b5c966db28b3670b810cbfef8ffa0044515a
Harpo Harbert <ryan_harbert@wgbh.org>
2024-05-21 15:21:14 -0700

    Adds AAPBRecordBlock `start_time` and `end_time`

--- ov_collections/blocks.py
+++ ov_collections/blocks.py
@@ -1,5 +1,8 @@
+from django.utils.dateparse import parse_duration
+from django.utils.functional import cached_property
 from wagtail.blocks import (
     BooleanBlock,
+    FieldBlock,
     RichTextBlock,
     StructBlock,
     TextBlock,
@@ -8,14 +11,45 @@ from wagtail.blocks import (
 from wagtail.images.blocks import ImageChooserBlock
 
 
+class DurationBlock(FieldBlock):
+    def __init__(
+        self, required=True, help_text=None, format=None, validators=(), **kwargs
+    ):
+        self.field_options = {
+            "required": required,
+            "help_text": help_text,
+            "validators": validators,
+        }
+        self.format = format
+        super().__init__(**kwargs)
+
+    @cached_property
+    def field(self):
+        from django.forms import DurationField
+
+        field_kwargs = {}
+        # TODO: Add an AdminDurationInput widget
+        field_kwargs.update(self.field_options)
+        return DurationField(**field_kwargs)
+
+    def to_python(self, value):
+        from datetime import timedelta
+
+        if value is None or isinstance(value, timedelta):
+            return value
+        else:
+            return parse_duration(value)
+
+    class Meta:
+        icon = "time"
+
+
 class ContentBlock(StructBlock):
     """Generic External link block
-
-    This is the base block for a generic external link. All fields are required
-
-    Attributes:
-        title: RichTextBlock with italics only
-        link: URLBlock
+    )
+    from wagtail.images.blocks import ImageChooserBlock
+            title: RichTextBlock with italics only
+            link: URLBlock
 
     """
 
@@ -53,6 +87,8 @@ class AAPBRecordsBlock(StructBlock):
         show_title: Show the title of records on the page
         show_thumbnail: Show the thumbnail of records on the page
         title: Optional title of the group
+        start_time: Optional start time for the group
+        end_time: Optional end time for the group
     """
 
     guids = TextBlock(
@@ -74,3 +110,27 @@ class AAPBRecordsBlock(StructBlock):
         help_text='The title of this group',
         features=['italic'],
     )
+
+    start_time = DurationBlock(
+        required=False,
+        help_text='Start time for the group',
+    )
+
+    end_time = DurationBlock(
+        required=False,
+        help_text='End time for the group',
+    )
+
+    def clean(self, value):
+        data = super(AAPBRecordsBlock, self).clean(value)
+
+        # Ensure that start_time is before end_time
+        if (
+            data.get('start_time')
+            and data.get('end_time')
+            and data['start_time'] > data['end_time']
+        ):
+            from django.core.exceptions import ValidationError
+
+            raise ValidationError('Start time must be before end time')
+        return data
