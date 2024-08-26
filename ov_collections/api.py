52c1f805d3347670506bfbe526f5c48d3404b372
Harpo Harbert <ryan_harbert@wgbh.org>
2024-08-26 15:23:47 -0700

    Includes `last_published_at` for collections API

--- ov_collections/api.py
+++ ov_collections/api.py
@@ -8,11 +8,16 @@ from .models import Collection
 class CollectionAPIViewSet(PagesAPIViewSet):
     model = Collection
 
+    meta_fields: ClassVar[list[str]] = PagesAPIViewSet.meta_fields + [
+        'last_published_at',
+    ]
+
     listing_default_fields: ClassVar[list[str]] = [
         *PagesAPIViewSet.listing_default_fields,
         'title',
         'introduction',
         'cover_image',
+        'last_published_at',
     ]
 
     def get_queryset(self):
