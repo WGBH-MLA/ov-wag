54458cd4438f2ed6d806acbd0b05b24812592adb
Harpo Harbert <ryan_harbert@wgbh.org>
2024-12-11 14:49:39 -0800

    Try: get_hero_thumb_url

--- exhibits/models.py
+++ exhibits/models.py
@@ -188,15 +188,17 @@ class ExhibitPage(HeadlessMixin, Page):
 
     featured = models.BooleanField(default=False)
 
+    def get_hero_thumb_url(self):
+        if self.hero_image:
+            return self.hero_image.rendition('fill-480x270').url
+        return ''
+
     search_fields: ClassVar[list[index.SearchField]] = [
         *Page.search_fields,
         index.AutocompleteField('body'),
         index.FilterField('featured'),
         index.SearchField('slug'),
-        index.SearchField(
-            'hero_image',
-            serializer=ImageRenditionField('fill-480x270', source='hero_image'),
-        ),
+        index.SearchField('get_hero_thumb_url'),
     ]
 
     content_panels: ClassVar[list[FieldPanel]] = [
