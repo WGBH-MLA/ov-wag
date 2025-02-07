fa25eddc0aeb6beeb3849112ba3cc434e66b0c55
Harpo Harbert <ryan_harbert@wgbh.org>
2025-02-07 08:31:50 -0800

    Add `/health` status route Fixes #238

--- ov_wag/api.py
+++ ov_wag/api.py
@@ -9,6 +9,7 @@ from wagtail_headless_preview.models import PagePreview
 from authors.api import AuthorsAPIViewSet
 from exhibits.api import ExhibitsAPIViewSet, FeaturedExhibitsAPIViewSet
 from ov_collections.api import CollectionAPIViewSet
+from ov_wag.health import HealthAPIViewSet
 
 # Create the router. 'wagtailapi' is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
@@ -44,16 +45,16 @@ class PagePreviewAPIViewSet(PagesAPIViewSet):
         return page
 
 
-api_router.register_endpoint('page_preview', PagePreviewAPIViewSet)
-
 # Add the three endpoints using the 'register_endpoint' method.
 # The first parameter is the name of the endpoint (eg. pages, images). This
 # is used in the URL of the endpoint
 # The second parameter is the endpoint class that handles the requests
-api_router.register_endpoint('pages', PagesAPIViewSet)
-api_router.register_endpoint('images', ImagesAPIViewSet)
-api_router.register_endpoint('documents', DocumentsAPIViewSet)
 api_router.register_endpoint('authors', AuthorsAPIViewSet)
-api_router.register_endpoint('exhibits', ExhibitsAPIViewSet)
 api_router.register_endpoint('collections', CollectionAPIViewSet)
+api_router.register_endpoint('documents', DocumentsAPIViewSet)
+api_router.register_endpoint('exhibits', ExhibitsAPIViewSet)
 api_router.register_endpoint('featured', FeaturedExhibitsAPIViewSet)
+api_router.register_endpoint('health', HealthAPIViewSet)
+api_router.register_endpoint('images', ImagesAPIViewSet)
+api_router.register_endpoint('page_preview', PagePreviewAPIViewSet)
+api_router.register_endpoint('pages', PagesAPIViewSet)
