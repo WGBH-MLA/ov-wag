d66e6b314f597c571ead3d0f0817737a4fd53011
Erica Titkemeyer <erica_titkemeyer@wgbh.org>
2026-06-26 12:47:07 -0400

    removes Help field - request to remove this and not include in migration either

--- aapb_collections/models.py
+++ aapb_collections/models.py
@@ -10,14 +10,14 @@ from wagtail.api import APIField
 
 
 class SortChoices(models.TextChoices):
-    TITLE = 'title', 'Title'
-    DATE = 'date', 'Date'
-    RANDOM = 'random', 'Random'
+    TITLE = "title", "Title"
+    DATE = "date", "Date"
+    RANDOM = "random", "Random"
 
 
 class SortOrder(models.TextChoices):
-    ASCENDING = 'asc', 'Ascending'
-    DESCENDING = 'desc', 'Descending'
+    ASCENDING = "asc", "Ascending"
+    DESCENDING = "desc", "Descending"
 
 
 class AAPBCollection(BaseCollection):
@@ -26,10 +26,10 @@ class AAPBCollection(BaseCollection):
     """
 
     class Meta:
-        verbose_name = 'AAPB Collection'
-        verbose_name_plural = 'AAPB Collections'
+        verbose_name = "AAPB Collection"
+        verbose_name_plural = "AAPB Collections"
 
-    parent_page_types: ClassVar[list[str]] = ['home.AAPBHomePage']
+    parent_page_types: ClassVar[list[str]] = ["home.AAPBHomePage"]
     subpage_types: ClassVar[list[str]] = []
 
     # Fields
@@ -37,28 +37,27 @@ class AAPBCollection(BaseCollection):
     content = StreamField(
         [
             (
-                'background',
-                RichTextBlock(label='Collection Background', icon='doc-full-inverse'),
+                "background",
+                RichTextBlock(label="Collection Background", icon="doc-full-inverse"),
             ),
-            ('funders', RichTextBlock(icon='group')),
-            ('help', RichTextBlock(icon='help')),
+            ("funders", RichTextBlock(icon="group")),
             (
-                'resources',
-                RichTextBlock(label='Other Resources', icon='doc-full-inverse'),
+                "resources",
+                RichTextBlock(label="Other Resources", icon="doc-full-inverse"),
             ),
             (
-                'terms',
-                RichTextBlock(label='Suggested Searches', icon='doc-full-inverse'),
+                "terms",
+                RichTextBlock(label="Suggested Searches", icon="doc-full-inverse"),
             ),
-            ('timeline', RichTextBlock(icon='clock')),
+            ("timeline", RichTextBlock(icon="clock")),
         ]
     )
 
     featured_items = StreamField(
         [
-            ('records', AAPBRecordsBlock(icon='doc-full-inverse')),
+            ("records", AAPBRecordsBlock(icon="doc-full-inverse")),
         ],
-        help_text='Featured items in the collection',
+        help_text="Featured items in the collection",
         blank=True,
     )
 
@@ -66,7 +65,7 @@ class AAPBCollection(BaseCollection):
         max_length=8,
         choices=SortChoices.choices,
         default=SortChoices.TITLE,
-        help_text='Choose how to sort the collection items.',
+        help_text="Choose how to sort the collection items.",
         blank=True,
         null=True,
     )
@@ -75,7 +74,7 @@ class AAPBCollection(BaseCollection):
         max_length=4,
         choices=SortOrder.choices,
         default=SortOrder.ASCENDING,
-        help_text='Choose the order of the sorted items.',
+        help_text="Choose the order of the sorted items.",
         blank=True,
         null=True,
     )
@@ -84,19 +83,19 @@ class AAPBCollection(BaseCollection):
 
     content_panels: ClassVar[list[FieldPanel]] = [
         *BaseCollection.content_panels,
-        FieldPanel('content'),
+        FieldPanel("content"),
         MultiFieldPanel(
-            [FieldPanel('featured_items')],
-            heading='Featured Items',
-            help_text='Featured items in the collection, such as records or exhibits.',
+            [FieldPanel("featured_items")],
+            heading="Featured Items",
+            help_text="Featured items in the collection, such as records or exhibits.",
         ),
         MultiFieldPanel(
             [
-                FieldPanel('sort_by'),
-                FieldPanel('sort_order'),
+                FieldPanel("sort_by"),
+                FieldPanel("sort_order"),
             ],
-            heading='Sorting Options',
-            help_text='Choose how to sort the collection items.',
+            heading="Sorting Options",
+            help_text="Choose how to sort the collection items.",
         ),
     ]
 
@@ -106,16 +105,16 @@ class AAPBCollection(BaseCollection):
 
     api_fields: ClassVar[list[APIField]] = [
         *BaseCollection.api_fields,
-        APIField('content'),
-        APIField('featured_items'),
-        APIField('sort_by'),
-        APIField('sort_order'),
+        APIField("content"),
+        APIField("featured_items"),
+        APIField("sort_by"),
+        APIField("sort_order"),
         APIField(
-            'cover_medium',
-            serializer=ImageRenditionField('fill-800x800', source='cover_image'),
+            "cover_medium",
+            serializer=ImageRenditionField("fill-800x800", source="cover_image"),
         ),
         APIField(
-            'cover_small',
-            serializer=ImageRenditionField('fill-400x400', source='cover_image'),
+            "cover_small",
+            serializer=ImageRenditionField("fill-400x400", source="cover_image"),
         ),
     ]
